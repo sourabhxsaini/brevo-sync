0bcfd036b62fed0b6ff3f5f82d28010187acc72b
SOURABH <sainisourav7900@gmail.com>
2026-05-04 00:47:45 +0530

    Change export syntax from ES6 to CommonJS

--- sync-mobile.js
+++ sync-mobile.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Only allow POST
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -62,4 +62,4 @@ export default async function handler(req, res) {
       detail: errorDetail
     });
   }
-}
+};
