041c9c82cdd14271f0b912308c015103918f2a3a
SOURABH <sainisourav7900@gmail.com>
2026-05-04 00:47:56 +0530

    Change export syntax from ES6 to CommonJS

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     status: 'ok',
     service: 'Brevo SMS → MOBILEPHONE Sync',
     webhook_url: '/api/sync-mobile',
     method: 'POST'
   });
-}
+};
